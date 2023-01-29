fb65b7758468249f9e2251f2eb82f67560452481
BrassiCold <brassicold@outlook.com>
2023-01-29 17:34:50 +0800

    Initial commit

---
+++ src/main/kotlin/fun/brassicold/brassi/alchemy/project/BrassiAlchemy.kt
@@ -0,0 +1,11 @@
+package `fun`.brassicold.brassi.alchemy.project
+
+import taboolib.common.platform.Plugin
+import taboolib.common.platform.function.info
+
+object BrassiAlchemy : Plugin() {
+
+    override fun onEnable() {
+        info("Successfully running ExamplePlugin!")
+    }
+}
